9b8539d3c7c03056bd30cdf1f7c9d38177293c55
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-05-13 09:41:17 +0530

    created custom logic for filling collection items and added new collection option

--- src/components/CollectionDetail/CollectionDetail.js
+++ src/components/CollectionDetail/CollectionDetail.js
@@ -1,15 +1,23 @@
 import React from "react";
 import styles from "./CollectionDetail.module.css";
 import CollectionDetailItem from "./CollectionDetailItem";
+import { collectionsList } from "./helpers";
+import add_collection_icon from "../../assets/add_collection.png";
 
 const CollectionDetail = () => {
   return (
     <div className={styles.collectionDetailMain}>
-      <CollectionDetailItem></CollectionDetailItem>
-      <div>hidfd</div>
-      <div>hfsdfdsi</div>
-      <div>hfdsfdsi</div>
-      <div>haaai</div>
+      {collectionsList.map((item, index) => (
+        <CollectionDetailItem
+          key={index}
+          title={item.title}
+          description={item.description}
+          stats={item.stats}
+        ></CollectionDetailItem>
+      ))}
+      <div className={styles.addCollectionItem}>
+        <img src={add_collection_icon} alt="add new collection item"></img>
+      </div>
     </div>
   );
 };
--- src/components/CollectionDetail/CollectionDetailItem.js
+++ src/components/CollectionDetail/CollectionDetailItem.js
@@ -4,26 +4,24 @@ import download_icon from "../../assets/download.png";
 import box_icon from "../../assets/box.png";
 import delete_icon from "../../assets/delete.png";
 
-const CollectionDetailItem = () => {
+const CollectionDetailItem = ({ title, description, stats }) => {
   return (
     <div className={styles.collectionDetailItemCard}>
       <div className={styles.collectionDetailItemToolbar}>
         <button className={styles.downloadButton}>
-          Download data <img src={download_icon}></img>
+          Download data <img src={download_icon} alt="download data"></img>
         </button>
         <div className={styles.statsInfo}>
           <button className={styles.statsButton}>
-            87<img src={box_icon}></img>
+            {stats}
+            <img src={box_icon} alt="collection stats"></img>
           </button>
-          <img src={delete_icon}></img>
+          <img src={delete_icon} alt="delete"></img>
         </div>
       </div>
       <div className={styles.collectionDetailItem}>
-        <div className={styles.collectionDetailItemTitle}>My Collection</div>
-        <div className={styles.collectionDetailItemDesc}>
-          Collection Description (Extended) that is added by user when creating
-          the collection to inform users of the content etc.
-        </div>
+        <div className={styles.collectionDetailItemTitle}>{title}</div>
+        <div className={styles.collectionDetailItemDesc}>{description}</div>
       </div>
     </div>
   );
